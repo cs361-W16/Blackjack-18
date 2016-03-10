71d5197069841bff42c2fe996bc247ddcff4e4fc
siebertc <siebertc@oregonstate.edu>
2016-03-10 02:30:42 -0800

    Created UML design classes
    Created UML Design classes as described in the UML Wiki

---
+++ src/main/java/models/basePlayer.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.util.ArrayList;
+/**
+ * Created by Charles Siebert on 3/9/2016.
+ */
+public class basePlayer {
+
+    public String name;
+
+    public.java.util.List<Card> hand = new ArrayList<>();
+
+    //abstract method
+    public void hit() {}
+
+
+
+}
